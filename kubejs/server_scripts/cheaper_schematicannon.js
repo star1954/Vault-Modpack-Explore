530f092056bff1653c0f4537de0533074a417288
star1954 <andidiwang@yahoo.com>
2024-07-08 21:01:34 -0700

    Edited Eureka Configs(airships are slightly slower now) Added cheaper schematicannon recipe
    Signed-off-by: star1954 <andidiwang@yahoo.com>

---
+++ kubejs/server_scripts/cheaper_schematicannon.js
@@ -0,0 +1,10 @@
+ServerEvents.recipes(event => {
+    event.shaped('create:schematicannon',[
+        ' S ',
+        'S S',
+        'WWW',
+    ],{
+        S: '#minecraft:stone_tool_materials',
+        W: '#minecraft:planks'
+    });
+});
